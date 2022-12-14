5cab5c1577afbe53fe3eb1c4f95328bf302dd429
umeshuy <110802360+umeshuy@users.noreply.github.com>
2022-12-14 13:35:37 +0530

    added form details to local storage

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('hello');
+function submit(){
+var userName = documnet.getElementById('name').value;
+var email = documnet.getElementById('email').value;
+
+var abc = localStorage.setItem('name', userName);
+var def = localStorage.setItem('email', email);
+}
