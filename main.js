798c51c63eae723f666ec2dd5f2853466c9dad51
umeshuy <110802360+umeshuy@users.noreply.github.com>
2022-12-14 16:24:59 +0530

    editbutton added

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ var email = documnet.getElementById('email').value;
 var abc = localStorage.setItem('name', userName);
 var def = localStorage.setItem('email', email);
 }
+
+//delete function
+function deleteUser(email){
+    localStorage.removeItem(email);
+    removeUser(email);
+}
+
+function removeUser(email){
+    var parent = document.getElementById('myforms');
+    var child = document.getElementById('');
+    if(child){
+        parent.removeChild(child);
+    }
+}
+
+function editDetails(email,name){
+document.getElementById('name').value = name;
+document.getElementById('email').value = email;
+removeUser(email);
+}
